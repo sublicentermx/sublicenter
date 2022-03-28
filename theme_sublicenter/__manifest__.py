ac81bfe4414b45e9b2df84c452b17f17e9bc5977
sublicentermx <sublicentermx@gmail.com>
2022-03-28 20:13:45 +0000

    v1.1

--- theme_sublicenter/__manifest__.py
+++ theme_sublicenter/__manifest__.py
@@ -1,11 +1,16 @@
 {
-    'name': 'Tutorial theme',
+    'name': 'Theme SublicenterMX',
     'description': 'A description for your theme.',
     'version': '1.0',
-    'author': 'Your name',
+    'author': 'Rubén Ely Trujillo Pérez',
     'category': 'Theme/Creative',
 
     'depends': ['website', 'website_sale'],
     'data': [
     ],
+    'assets': {
+        'web.assets_frontend' [
+            'theme_sublicenter/static/scss/main.scss'
+        ]
+    }
 }
