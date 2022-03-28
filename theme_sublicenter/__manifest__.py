a9734e66cc2b268b15f4407e829a90097ae2fe00
sublicentermx <sublicentermx@gmail.com>
2022-03-28 18:58:37 +0000

    My first theme

---
+++ theme_sublicenter/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Tutorial theme',
+    'description': 'A description for your theme.',
+    'version': '1.0',
+    'author': 'Your name',
+    'category': 'Theme/Creative',
+
+    'depends': ['website', 'website_sale'],
+    'data': [
+    ],
+}
